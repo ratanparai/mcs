ca5ef0c71bddb7b30c439a1bc60026d32d1fff32
Ratan Sunder Parai <ratanparai@gmail.com>
2018-06-03 16:08:38 +0600

    add barebone auto generated springboot template

---
+++ catalog/src/main/kotlin/com/ratanparai/msc/api/catalog/CatalogApplication.kt
@@ -0,0 +1,11 @@
+package com.ratanparai.msc.api.catalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogApplication
+
+fun main(args: Array<String>) {
+    runApplication<CatalogApplication>(*args)
+}
